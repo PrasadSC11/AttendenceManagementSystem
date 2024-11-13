c4bcb446b2d447d1414b7388c6861343c439dec8
PrasadSC1 <112806714+PrasadSC1@users.noreply.github.com>
2024-11-13 11:31:25 +0530

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
+import { WebcamModule } from 'ngx-webcam';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { AttendanceComponent } from './attendence/attendence.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AttendanceComponent
   ],
   imports: [
     BrowserModule,
+    WebcamModule,
+    RouterModule, HttpClientModule,
     AppRoutingModule
   ],
   providers: [
